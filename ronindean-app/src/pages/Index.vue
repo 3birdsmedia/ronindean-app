caa9cdd313de6d5038282206ef9847425e9dace6
Marco Segura <marco@leadid.net>
2020-02-14 02:09:41 -0800

    Finished hero, services, about

--- ronindean-app/src/pages/Index.vue
+++ ronindean-app/src/pages/Index.vue
@@ -39,24 +39,27 @@
             <span>• EXECUTIVE • PERSONAL • CONCIERGE • TEAM • TALENT/INFLUENCERS •</span>
         </div>
     </div-->
-    <div class="hero main-section window-height window-width text-center flex items-center row justify-center" ref="hero">
+    <div class="hero main-section window-width text-center flex items-center row justify-center" ref="hero">
     <!-- <q-parallax :height="wh">
       <template v-slot:media>
         <video :height="wh" poster="https://cdn.quasar.dev/img/polina.jpg" autoplay loop muted>
           <source type="video/mp4" src="~assets/blurred-hero.mp4">
         </video>
       </template> -->
-        <div class="col-8 col-lg-6">
+        <div class="col-11 col-sm-8 col-lg-6 q-pt-xl q-pt-none-sm">
           <div class="full-width">
             <img alt="RoninDean Enterprises Logo" src="~assets/ronindean-logo-full.svg" class="logo">
             <h2 class="full-width q-ma-none text-italic anim" v-in-viewport>Your exceptional and elegant virtual assistant, supporting you the right way</h2>
-            <q-btn  @click="scrollMeTo('services')">Learn More</q-btn>
-
+          </div>
+        </div>
+        <div class="full-width flex row">
+          <div class="col-12 col-md-6 q-px-lg divider anim" v-in-viewport>
+            <q-btn class="q-ma-lg q-px-md" size="lg" @click="scrollMeTo('services')" v-ripple>Learn More</q-btn>
             <p>Tailored assistant services to give you time back where you need it most executives, entrepreneurs, teams, talent/influencers.</p>
-            <q-btn  @click="scrollMeTo('contact')">Request a meeting</q-btn>
+          </div>
+          <div class="col-12 col-md-6 q-px-lg anim" v-in-viewport>
+            <q-btn class="q-ma-lg q-px-md" size="lg" @click="scrollMeTo('contact')" v-ripple>Request a meeting</q-btn>
             <p>Allowing executives, entrepreneurs, teams, talent and influencers to focus on their goals</p>
-
-            <!--span class="tag self-end hide">• EXECUTIVE • PERSONAL • CONCIERGE • TEAM • TALENT/INFLUENCERS •</span-->
           </div>
         </div>
         <div class="motif large anim-up" v-in-viewport></div>
@@ -64,8 +67,8 @@
     <!-- </q-parallax> -->
     </div>
     <div class="main-section window-width parallax services row justify-center" ref="services" id="services">
-          <h2 class="full-width text-center anim" v-in-viewport>SERVICES</h2>
-          <p class="col-10 anim-up" v-in-viewport>
+          <h2 class="full-width text-center anim q-pb-xl" v-in-viewport>SERVICES</h2>
+          <p class="col-10 anim-up q-pb-xl" v-in-viewport>
             What could you do with an extra two hours in your day? Or twenty hours in your week, every week? Hit your rank? Build your team? Hit that next goal? Call your mom? Pick up your kids from school? Take real vacation time? Get more sleep? From our experience supporting business owners, CEO’s and social media influencers we know your back office is flooded, your phone is chiming constantly and your trying to keep all the juggling balls in the air, because dropping one means dropping them all. Outsourcing particular tasks and seeking support from a helping hand or even “another you” is a need you can’t afford to not have. We’ve designed our service model knowing one size doesn’t fit all.</p>
         <div class="row justify-between q-col-gutter-lg q-pa-xl items-strech">
           <div class="col-12 col-sm-6 col-lg-3 service-list executive anim-delay" v-in-viewport>
@@ -121,28 +124,30 @@
             </ul>
           </div>
           </div>
-          <p class="full-width q-ma-none text-italic anim-delay in-viewport"  v-in-viewport> As a client-focused firm, you tell us what you need so we can see that need and meet it; delivering exactly what you pay for and more. So again, we ask, what would you do with an extra two hours in your day?</p>
+          <p class="full-width q-ma-none text-center text-large q-pt-xl q-pb-md text-italic anim-delay in-viewport"  v-in-viewport> As a client-focused firm, you tell us what you need so we can see that need and meet it; delivering exactly what you pay for and more. So again, we ask, what would you do with an extra two hours in your day?</p>
       </div>
     </div>
-    <div class="main-section window-width flex items-center row justify-center" ref="about" id="about">
+    <div class="main-section window-width flex items-center row justify-center about" ref="about" id="about">
       <div class="col-10 col-md-4 text-center anim-down q-py-lg" v-in-viewport>
         <q-img
           src="~assets/kirstie-bw.png"
           spinner-color="white"
           style="width: 90%; border-radius:500px"
-          img-class="my-custom-image"
+          img-class="responsive-image"
           alt="Picture of Kirtie Segura"
         >
         </q-img>
       </div>
       <div class="col-10 col-md-6 text-center anim-up" v-in-viewport>
         <h2>WORK SMARTER NOT HARDER</h2>
-        <p>No "One Size Fits All"<br/>
-              ~OUR ASSISTANCE IS TAILOR-MADE TO YOUR NEEDS<br/>
-              ~WE CATER TO YOU: THE ENTREPRENEUR, THE SMALL BUSINESS, THE INFLUENCER<br/>
-              ~DEDICATED TO EXCELLENCE, QUALITY & VALUE ("DONE" IS NOT "BETTER THAN PERFECT")</p>
+          <ul>
+            <li>No "One Size Fits All"</li>
+            <li>Our assistance is tailor-made to your needs</li>
+            <li>We cater to you: The Entrepreneur, The Small Business, The Influencer</li>
+            <li>Dedicated to excellence, quality & value ("done" is not "better than perfect")</li>
+          </ul>
         <h2>PLEASURE TO MEET YOU</h2>
-        <p class="text-italic">I'M JOINING YOUR TEAM</p>
+        <p class="text-italic text-large">I'M JOINING YOUR TEAM</p>
         <p>With 18 years experience in customer care, executive operations and administration. LIVING BY  the core values to live with purpose and be of service to others! During my many years working behind the scenes with direct sales entrepreneurs and business owners, I continuously heard "I need another me". Now they say, "I NEED ANOTHER YOU!"</p>
       </div>
       <div class="motif large anim-up" v-in-viewport></div>
@@ -150,43 +155,87 @@
     </div>
     <div class="main-section window-width flex items-center row justify-center parallax pricing" ref="pricing" id="pricing">
       <div class="anim" v-in-viewport>
-        <h2 class="full-width text-center q-mb-none">PRICING</h2>
-        <p>Pricing is customized to your needs, time required and length of service. Prices below are for reference based on calendar week and subject to change.</p>
+        <h2 class="full-width text-center anim q-pb-lg" v-in-viewport>PRICING</h2>
+        <p class="text-large q-px-xl">Pricing is customized to your needs, time required and length of service. Prices below are for reference based on calendar week and subject to change.</p>
       </div>
       <div class=" q-px-lg row items-center">
-        <div class="row">
-          <div class="anim-up" v-in-viewport>
-            <h3 class="q-ma-none q-pt-none q-pt-lg">Hands To Serve</h3>
-            <p>Small weekly tasks and projects to give you the breathing room you need to focus on your priorities<br/>
-              <span class="text-italic">Starting At $350/week</span>
-            </p>
+        <div class="row justify-center col-10">
+          <div class="flex items-center row justify-center anim price q-pa-xl-md q-pa-sm" v-in-viewport>
+            <div class="col-12 col-sm-4">
+              <q-img
+                src="~assets/helpingprice.jpg"
+                spinner-color="white"
+                style="border-radius:500px"
+                img-class="responsive-image"
+                alt="Picture of Kirtie Segura"
+              >
+              </q-img>
+            </div>
+            <div class="col-12 col-sm-8 q-px-lg">
+              <h3 class="q-ma-none q-pt-lg">Hands To Serve</h3>
+              <p>Small weekly tasks and projects to give you the breathing room you need to focus on your priorities<br/>
+                <span class="text-italic">Starting At $350/week</span>
+              </p>
+            </div>
           </div>
-          <div class="anim-up" v-in-viewport>
-          <h3 class="q-ma-none q-pt-none q-pt-lg">Another Me</h3>
-          <p>Working as you or for you to interact with your team, inbox and to-do list to free you up for the freedoms you were originally after.<br/>
-            <span class="text-italic">Starting At $700/week</span>
-          </p>
+          <div class="flex items-center row justify-center anim price q-pa-xl-md q-pa-sm" v-in-viewport>
+            <div class="col-12 col-sm-8 q-px-lg text-right">
+              <h3 class="q-ma-none q-pt-lg">Another Me</h3>
+              <p>Working as you or for you to interact with your team, inbox and to-do list to free you up for the freedoms you were originally after.<br/>
+              <span class="text-italic">Starting At $700/week</span>
+              </p>
+            </div>
+            <div class="col-12 col-sm-4">
+              <q-img
+                src="~assets/anotherprice.jpg"
+                spinner-color="white"
+                style="border-radius:500px"
+                img-class="responsive-image"
+                alt="Picture of Kirtie Segura"
+              >
+              </q-img>
+            </div>
           </div>
-          <div class="anim-up" v-in-viewport>
-            <h3 class="q-ma-none q-pt-none q-pt-lg">Executive/Personal</h3>
-            <p>Operational and strategic assistants needed to run your business or life on your terms.<br/>
-              <span class="text-italic">Starting At $925/week</span></p>
+          <div class="flex items-center row justify-center anim price q-pa-xl-md q-pa-sm" v-in-viewport>
+            <div class="col-12 col-sm-4">
+              <q-img
+                src="~assets/executiveprice.jpg"
+                spinner-color="white"
+                style="border-radius:500px"
+                img-class="responsive-image"
+                alt="Picture of Kirtie Segura"
+              >
+              </q-img>
+            </div>
+            <div class="col-12 col-sm-8 q-px-lg">
+              <h3 class="q-ma-none q-pt-lg">Executive/Personal</h3>
+              <p>Operational and strategic assistants needed to run your business or life on your terms.<br/>
+                <span class="text-italic">Starting At $925/week</span></p>
+            </div>
           </div>
-          <div class="anim-up" v-in-viewport>
-          <h3 class="q-ma-none q-pt-none q-pt-lg">Shared Assistant</h3>
-            <p>Built to support your team’s collective and individual needs to ensure synergy and consistency within the organization.<br/>
+          <div class="flex items-center row justify-center anim price q-pa-xl-md q-pa-sm" v-in-viewport>
+            <div class="col-12 col-sm-8 q-px-lg">
+              <h3 class="q-ma-none q-pt-lg">Shared Assistant</h3>
+              <p>Built to support your team’s collective and individual needs to ensure synergy and consistency within the organization.<br/>
               <span class="text-italic">Starting At $200 Per Person Per Week</span>
-            </p>
+              </p>
+            </div>
+            <div class="col-12 col-sm-4">
+              <q-img
+                src="~assets/sharedprice.jpg"
+                spinner-color="white"
+                style="border-radius:500px"
+                img-class="responsive-image"
+                alt="Picture of Kirtie Segura"
+              >
+              </q-img>
+            </div>
           </div>
-        </div>
-        <div class="col">
-
         </div>
       </div>
-        <div class="anim-up col-12" v-in-viewport>
-          <span class="">Project & Hourly Rates Available</span>
-        </div>
-
+      <div class="anim-up col-12" v-in-viewport>
+        <span class="">Project & Hourly Rates Available</span>
+      </div>
     </div>
     <div class="main-section window-width flex items-center row justify-center" ref="contact" id="contact">
       <div class="col-10 col-md-6">
